4c5d4d68d43265d151ffd305615330af4b9d23c5
Matthew Clair <Canada.Rox.Gh@gmail.com>
2011-11-14 15:44:18 -0500

    added until charged replaced

--- _scripts/infobar/batt_info.sh
+++ _scripts/infobar/batt_info.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-acpi | cut -f3- -d' ' | sed 's/ remaining//' | sed 's/Discharging, //' | sed 's/Charging, /C/'
+acpi | cut -f3- -d' ' | sed 's/ remaining//' | sed 's/Discharging, //' | sed 's/Charging, /C: /' | sed 's/ until charged//'
 exit 0
