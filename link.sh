353f60515e8867419682e6c8a064deaec669548a
Matthew Clair <canada.rox.gh@gmail.com>
2012-06-21 22:11:46 -0400

    Updated some stuff

--- link.sh
+++ link.sh
@@ -4,9 +4,14 @@ for FILE in _*
 do
 	LINKNAME=$(echo $FILE | tr -d _)
 	LINKNAME=.$LINKNAME
-	if [ -a $HOME/$LINKNAME ]
+	if [ -a $HOME/$LINKNAME ] # if the file exists, only create the link if -b is specified
 	then
-		mv $HOME/$LINKNAME $HOME/$LINKNAME.bak
+		if [ "$1" = "-b" ]
+		then
+			mv $HOME/$LINKNAME $HOME/$LINKNAME.bak
+			ln -s $(pwd)/$FILE $HOME/$LINKNAME
+		fi
+	else
+		ln -s $(pwd)/$FILE $HOME/$LINKNAME
 	fi
-	ln -s $(pwd)/$FILE $HOME/$LINKNAME
 done 
